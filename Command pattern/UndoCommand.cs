9e07ddcb805c9f749dcc2e91729efa48ac9d45fe
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-07-09 20:22:47 +0300

    Add project files.

---
+++ Command pattern/UndoCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitedSizeStack.Command_pattern
+{
+    internal class UndoCommand<T> : ICommand
+    {
+        LimitedSizeStack<T> limitedSizeStack;
+
+        public UndoCommand(LimitedSizeStack<T> stack) 
+        {
+            limitedSizeStack = stack;
+        }
+        public bool CanUndo()
+        {
+            return limitedSizeStack.Count > 0;
+        }
+
+        public void Undo()
+        {
+            limitedSizeStack.Pop();
+        }
+    }
+}
---
+++ LimitedSizeStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+    private readonly int _limitSize;
+	public readonly LinkedList<T> _stack = new();
+    public int Count => _stack.Count;
+
+    public LimitedSizeStack(int undoLimit)
+	{
+		_limitSize = undoLimit;
+	}
+
+	public void Push(T item)
+	{
+		if (_limitSize == 0) return;
+		if (_stack.Count < _limitSize)
+		{
+			_stack.AddFirst(item);
+		}
+        else
+        {
+			_stack.RemoveLast();
+			_stack.AddFirst(item);
+        }
+	}
+
+	public T Pop()
+	{
+		var result = _stack.First.Value;
+		_stack.RemoveFirst();
+        return result;
+	}
+}
